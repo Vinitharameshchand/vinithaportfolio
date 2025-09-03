d04320f949d780ff32477d318e816b942e5bd1e5
vinitha <vinitha@vinithas-MacBook-Air.local>
2025-09-04 03:38:39 +0530

    Fix CSS paths and base in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,27 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   css:{
+//     modules:{
+//       localsConvention: "camelCase"
+//     }
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Ensures correct relative paths for deployment
   plugins: [react()],
-  css:{
-    modules:{
+  css: {
+    modules: {
       localsConvention: "camelCase"
     }
   }
 })
-
